a03fb40a7a77099577a661c2f37a8852dca5213d
Palomino34 <dat.tran@ghielectronics.com>
2021-08-09 16:27:37 -0400

    Updates.

--- main.ts
+++ main.ts
@@ -1,1 +1,39 @@
+/**
+ * Outputs.
+ */
+//% groups='["other", "Colors"]'
+//% color="#FF00ff" weight=97 icon="\uf205"
+namespace mytestext {
+	/**
+    * Turns on or off the mytestext
+    */
+    //% blockId=mytestext_set block="set mytestext to %on=toggleHighLow"
+    //% weight=89
+    //% blockGap=8
+    export function setmytestext(on: boolean) {
+        //__setmytestext(on);
+    }
 
+    /**
+    * Turns off the mytestext
+    */
+    //% blockId=mytestext_set_off block="mytestext off"
+    //% weight=89
+    export function off() {
+        //__setmytestext(false);
+    }
+
+	/**
+    * Turns on the mytestext
+    */
+    //% blockId=mytestext_set_on block="mytestext on"
+    //% weight=89
+    export function on() {
+        //__setmytestext(true);
+    }
+
+    function update() {
+
+    }
+
+}
