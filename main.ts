12ed7611b4fcd8f85c823133f21a9b4a2dc8ff13
Dat Tran <dat.tran@ghielectronics.com>
2021-08-09 16:34:41 -0400

    add c++

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ namespace mytestext {
     //% weight=89
     //% blockGap=8
     export function setmytestext(on: boolean) {
-        //__setmytestext(on);
+        __setmytestext(on);
     }
 
     /**
@@ -20,7 +20,7 @@ namespace mytestext {
     //% blockId=mytestext_set_off block="mytestext off"
     //% weight=89
     export function off() {
-        //__setmytestext(false);
+        __setmytestext(false);
     }
 
 	/**
@@ -29,7 +29,7 @@ namespace mytestext {
     //% blockId=mytestext_set_on block="mytestext on"
     //% weight=89
     export function on() {
-        //__setmytestext(true);
+        __setmytestext(true);
     }
 
     function update() {
